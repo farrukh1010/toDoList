52653ba5326be4ec26bcc55d41c992e0bede5874
farrukh1010 <42179299+farrukh1010@users.noreply.github.com>
2024-02-13 12:34:32 +0500

    first message

---
+++ index.ts
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+let todos:string[]=[ ];
+let loop =true;
+while(loop){
+    const answers:{
+        TODO:string,
+        addmore:boolean
+    }= await inquirer.prompt([
+        {
+            type:"input",
+            name:"TODO",
+            message:"what do you want to add in your todo?"
+
+        },
+        {
+            type:"confirm",
+            name:"addmore",
+            message:"Do ypoy want to add more todo?",
+            default:false
+        }
+    ])
+    const{TODO, addmore}= answers;
+    loop =addmore
+    if(TODO){
+  todos.push(TODO)
+    }else{
+        console.log("kindly add valid input")
+    }
+}
+// console.log(todos)
+if(todos.length >0){
+    console.log("Your Todo List: ")
+   todos.forEach(todo=>{
+    console.log(todo)
+   })
+}else{
+    console.log("No todos found")
+}
